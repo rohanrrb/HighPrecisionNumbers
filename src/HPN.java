fde1a3cb9f326531e2ef67661cf49ddd65fd26fd
Rohan Bopardikar <rohanbopi@gmail.com>
2023-03-16 04:17:14 -0700

    phased out error for isExact + precision

--- src/HPN.java
+++ src/HPN.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Objects;
 
 /**
- * 
+ * HIGH PRECISION NUMBER PROJECT (HPN)
  * @author Rohan Bopardikar
  * 
  *TO DO LIST 3/1:
@@ -16,13 +16,14 @@ import java.util.Objects;
  *				-does not change signs of original HPN
  *Multiplication Extra Zeros
  *Error handling for division
- *			-at what decimal val should non-terminating be rounded
+ *			-at what decimal value should non-terminating be rounded
  *			-add +1 at the very end, takes care of the rounding thing
  *Output format: +#.###±## or -#.###±###*
  *More efficient way to toString (there's a different structure: (x) ? (y)...\
  *Current sign handling for division is to complicated
  *Constructor when no decimal present
- *Make an add(HPN, int)?
+ *
+ *Arbitrary +1 error only for division. 
  *
  */
 public class HPN {
@@ -30,22 +31,40 @@ public class HPN {
 	private int intPart; 
 	private int[] fracPart; 
 	private int error; 
-	private boolean negative; //change to isNegative
-	private static int numCalculations = 0; 
-	private boolean errorIsTruncated;
+	private boolean isNegative; //change to isNegative
+	private boolean isTruncated;
+	
+	private boolean isExact; 
+	private int precision; 
+	
+	private static errorData errorData;
 	
 	@Override
 	public String toString() {
-		if(negative) {
-			if(errorIsTruncated) {
-				return "HPN: -" + intPart + "." + Arrays.toString(fracPart) + "(" + error +"*)";
-			}
-			return "HPN: -" + intPart + "." + Arrays.toString(fracPart) + "(" + error +")";
+		String exact = "";
+		if(isExact) {
+			exact = "exact";
 		}else {
-			if(errorIsTruncated) {
-				return "HPN: " + intPart + "." + Arrays.toString(fracPart) + "(" + error +"*)";
+			exact = "not exact";
+		}
+		
+		String fracNum = "";
+		
+		for(int i = 0; i < fracPart.length; i++) {
+			fracNum += fracPart[i];
+		}
+		
+		
+		if(isNegative) {
+			if(isTruncated) {
+				return "-" + intPart + "." + fracNum + "(" + exact  +")" + "("+precision+")*";
 			}
-			return "HPN: " + intPart + "." + Arrays.toString(fracPart) + "(" + error +")";
+			return "-" + intPart + "." + fracNum + "(" + exact +")" + "("+precision+")";
+		}else {
+			if(isTruncated) {
+				return  intPart + "." + fracNum + "(" + exact +")" + "("+precision+")*";
+			}
+			return  intPart + "." + fracNum + "(" + exact +")" + "("+precision+")";
 		}
 	}
 	
@@ -55,17 +74,10 @@ public class HPN {
 		this.intPart = a;
 		this.fracPart = new int[0];
 		this.error = 0; 
-		this.negative = false; 
+		this.isNegative = false; 
 		checkNegative(this);
-		
-	}
-	
-	public HPN(int a, int error) {
-		this.intPart = a;
-		this.fracPart = new int[0];
-		this.error = error; 
-		this.negative = false; 
-		checkNegative(this);
+		this.isExact = true; 
+		this.precision = 0; 
 		
 	}
 	
@@ -74,23 +86,30 @@ public class HPN {
 		this.intPart = a;
 		this.fracPart = b;
 		this.error = 0;
-		this.negative = false; 
+		this.isNegative = false; 
 		checkNegative(this);
+		this.isExact = true; 
+		this.precision = b.length; 
 	}
 	
 	
 	//String constructor
 	public HPN(String s) {
 		this.error = 0;
-		this.negative = false; 
+		this.isNegative = false; 
 		//split input
 		int decimalIndex = s.indexOf('.');
-		String intPart = s.substring(0, decimalIndex);
+		if(decimalIndex != 0) {
+			String intPart = s.substring(0, decimalIndex);
+			this.intPart = Integer.parseInt(intPart);
+		}else {
+			intPart = 0;
+		}
 		String fracPart = s.substring(decimalIndex + 1);
 		
 		int fracLength = fracPart.length(); 
 		
-		this.intPart = Integer.parseInt(intPart);
+		
 		this.fracPart = new int[fracLength];
 
 		//fills fracPart into array
@@ -98,18 +117,19 @@ public class HPN {
 			this.fracPart[i] = Integer.parseInt(fracPart.substring(i,i+1));
 		}
 		if(s.charAt(0) == '-') {
-			this.negative = true;
+			this.isNegative = true;
 			s = s.substring(1);
 			this.intPart *= -1;
 		}
 
-		
 		checkNegative(this);
-		
+		this.precision = this.fracPart.length;
+		this.isExact = true;
 	}
+	
 	public HPN(String s, int error) {
 		this.error = error;
-		this.negative = false; 
+		this.isNegative = false; 
 		//split input
 		int decimalIndex = s.indexOf('.');
 		String intPart = s.substring(0, decimalIndex);
@@ -125,15 +145,56 @@ public class HPN {
 			this.fracPart[i] = Integer.parseInt(fracPart.substring(i,i+1));
 		}
 		if(s.charAt(0) == '-') {
-			this.negative = true;
+			this.isNegative = true;
 			s = s.substring(1);
 			this.intPart *= -1;
 		}
 
-		
 		checkNegative(this);
+		this.precision = this.fracPart.length; 
+		this.isExact = true; 
 		
 	}
+	
+	public HPN(String s, boolean isExact) {
+		this.error = 0;
+		this.isNegative = false; 
+		//split input
+		int decimalIndex = s.indexOf('.');
+		
+		String fracPart = s.substring(decimalIndex + 1);
+		
+		int fracLength = fracPart.length(); 
+		
+		if(decimalIndex != 0) {
+			String intPart = s.substring(0, decimalIndex);
+			this.intPart = Integer.parseInt(intPart);
+		}else {
+			intPart = 0;
+		}
+		
+		
+		this.fracPart = new int[fracLength];
+
+		//fills fracPart into array
+		for(int i = 0; i < fracLength; i++) {
+			this.fracPart[i] = Integer.parseInt(fracPart.substring(i,i+1));
+		}
+		if(s.charAt(0) == '-') {
+			this.isNegative = true;
+			s = s.substring(1);
+			this.intPart *= -1;
+		}
+
+		checkNegative(this);
+		this.precision = this.fracPart.length; 
+		this.isExact = isExact; 
+		
+	}
+	
+	
+	
+	static final HPN zero = new HPN(0);
 	
 	
 	/**
@@ -146,9 +207,15 @@ public class HPN {
 		System.out.println(a.toString() + " + " + b);
 		HPN sum = new HPN(a.intPart + b); 
 		sum.fracPart = a.fracPart;
-		
-		sum.error = a.error + 1;
+		checkNegative(sum);
+		if(!a.isExact) {
+			sum.isExact = false;
+		}
 		return sum;
+	}
+	
+	public static HPN add(int a, HPN b) {
+		return add(b,a);
 	}
 	
 	
@@ -159,25 +226,24 @@ public class HPN {
 	 * @return HPN sum
 	 */
 	public static HPN add(HPN a, HPN b) {
-		numCalculations++;
 		int intSum = a.intPart + b.intPart;
 		HPN sum = new HPN(intSum);
 		
 		System.out.println(a.toString() + " + " + b.toString());
 		
-		if(a.negative && !b.negative) {
+		if(a.isNegative && !b.isNegative) {
 			return subtract(b, negate(copy(a)));
 		}
 		
-		if(!a.negative && b.negative) {
+		if(!a.isNegative && b.isNegative) {
 			return subtract(a, negate(copy(b)));
 		}
-		if(a.negative && b.negative) {
+		if(a.isNegative && b.isNegative) {
 			return negate(add(negate(copy(a)),negate(copy(b))));
 		}
 		
 		//processing
-		int addition[][] = normalize(a.fracPart, b.fracPart);
+		int addition[][] = padZeros(a.fracPart, b.fracPart);
 		
 		//initialize array for fracPart
 		int[] fracSum = new int[addition[0].length]; 
@@ -205,11 +271,17 @@ public class HPN {
 		sum.fracPart = fracSum;
 		//(sum);
 		
-		sum.error = ( HPNHPNerror(a,b, "add")).getErrorVal() + numCalculations;		
-		if(HPNHPNerror(a,b, "add").isTruncated()) {
-			sum.errorIsTruncated = true;
+		sum.error = ( errorData.addError(a,b, "add")).getErrorVal()  ;		
+		if(errorData.addError(a,b, "add").isTruncated()) {
+			sum.isTruncated = true;
 		}
-		System.out.println("NUM: " + numCalculations);
+		
+		if(!a.isExact || !b.isExact) {
+			sum.isExact = false;
+		}
+		
+		sum.precision = calculatePrecision(a,b);
+		applyPrecision(sum);
 		return sum;
 	}
 
@@ -227,12 +299,11 @@ public class HPN {
 	
 	
 	public static HPN subtract(HPN a, HPN b) {
-		numCalculations++;
 		System.out.println(a.toString() + " - " + b.toString());
 		
 		HPN result = new HPN(a.intPart - b.intPart);
 		
-		int[][] subtract = normalize(a.fracPart,b.fracPart);
+		int[][] subtract = padZeros(a.fracPart,b.fracPart);
 		int[] resultFrac = new int[subtract[0].length];
 		
 		for(int i = subtract[0].length - 1; i > 0; i--) {
@@ -264,16 +335,16 @@ public class HPN {
 		
 		checkNegative(result);
 		
-		if(result.negative) {
+		if(result.isNegative) {
 			return(negate(subtract(b,a))); 
 		}
 		
-		result.error = ( HPNHPNerror(a,b, "sub")).getErrorVal() + numCalculations;		
-		if(HPNHPNerror(a,b, "add").isTruncated()) {
-			result.errorIsTruncated = true;
+		result.error = ( errorData.addError(a,b, "sub")).getErrorVal();		
+		if(errorData.addError(a,b, "add").isTruncated()) {
+			result.isTruncated = true;
 		}
-		System.out.println("NUM: " + numCalculations);
 		
+		result.precision = calculatePrecision(a,b);
 		return result;
 	}
 	
@@ -283,26 +354,28 @@ public class HPN {
 	 * @param b
 	 * @return
 	 * Issue? Returns a 0 at the end for some cases ex) 20
+	 * Sign handling not updated
 	 */
 	public static HPN multiply(HPN a, int b) {
 		System.out.println(a.toString() + " * " + b);
+		int error = a.error;
 		
-		if(a.negative && b > 0) {
-			a.negative = false;
+		if(a.isNegative && b > 0) {
+			a.isNegative = false;
 			HPN result = multiply(a,b); 
-			result.negative = true;
+			result.isNegative = true;
 			return result;
 		}
 		
-		if (!a.negative && b<0) {
+		if (!a.isNegative && b<0) {
 			b *= -1;
 			HPN result = multiply(a,b); 
-			result.negative = true;
+			result.isNegative = true;
 			return result;
 		}
 		
-		if(a.negative && b < 0) {
-			a.negative = false; 
+		if(a.isNegative && b < 0) {
+			a.isNegative = false; 
 			b *= -1; 
 			return multiply(a,b);
 		}
@@ -325,13 +398,14 @@ public class HPN {
 				 fracProduct[i] = temp;
 				 temp = 0;
 			 }
-			 
-		 
 		}
-		
-		
-			
-		HPN product = new HPN(intProduct, fracProduct);		
+
+		HPN product = new HPN(intProduct, fracProduct);	
+		product.error = error;
+		if(b == 0) {
+			return zero;
+		}
+		product.error = error * b;
 		return product;
 	}
 	
@@ -342,28 +416,28 @@ public class HPN {
 	 * @return
 	 * NEEDS SOME MORE CASE TESTING...
 	 * BREAKS OFF REPEATING DECIMALS AT 10 WITH i
-	 * how should error work for this? clarify with dr. Heckman
+	 * how should error work for this? clarify with Dr. Heckman
 	 */
 	public static HPN divide(HPN a, int b) {
 		System.out.println(a.toString() + " / " + b);
+		int error = a.error + 1;
 		
-		if(a.negative && b < 0) {
-			a.negative = false;
+		if(a.isNegative && b < 0) {
+			a.isNegative = false;
 			b *= -1; 
-			
 			return divide(a,b);
 		}
 		
-		if(!a.negative && b < 0) {
+		if(!a.isNegative && b < 0) {
 			b *= -1;
 			HPN result = divide(a,b);
-			result.negative = true;
+			result.isNegative = true;
 			return result;
 		}
-		if(a.negative && b > 0) {
-			a.negative = false;
+		if(a.isNegative && b > 0) {
+			a.isNegative = false;
 			HPN result = divide(a,b);
-			result.negative = true;
+			result.isNegative = true;
 			return result;
 		}
 		
@@ -382,6 +456,8 @@ public class HPN {
 			HPN quotient = divide(a, b);
 			quotient.intPart += intQuotient; 
 			System.out.println("+" + intQuotient);
+			
+			
 			return quotient; 
 
 		}
@@ -418,52 +494,12 @@ public class HPN {
 				
 				i++;
 			}
-			quotient.fracPart = qFrac;
-
+		quotient.fracPart = qFrac;
+		quotient.error = error;
 		return quotient;
 	}
 	
-	public static errorArray HPNHPNerror(HPN a, HPN b, String operation) {
-		int error = 0;
-		boolean isTruncated = false; 
-		
-		int aLength = a.fracPart.length;
-		int bLength = b.fracPart.length;
 
-		double placeDiff;
-		int firstError = 0;
-		int secondError = 0;
-		
-		if(aLength == bLength) {
-			error = a.error + b.error;
-
-		}
-		else if(aLength > bLength){
-			placeDiff = aLength - bLength;
-			if(placeDiff > 9) {
-				placeDiff = 9;
-				isTruncated = true;
-			}
-			firstError = (int)(a.error * java.lang.Math.pow(10,placeDiff));
-			secondError = b.error;
-			error = firstError + secondError;
-		}
-		else {
-			placeDiff = bLength - aLength;
-			if(placeDiff > 9) {
-				placeDiff = 9;
-				isTruncated = true;
-				
-			}
-
-			firstError = (b.error * (int)java.lang.Math.pow(10,placeDiff));
-			secondError = a.error;
-			error = firstError + secondError;
-		}
-		
-		errorArray Error = new errorArray(error, isTruncated);
-		return Error;
-	}
 	
 	
 	public static int concat(int a, int b) {
@@ -492,7 +528,7 @@ public class HPN {
 	 * @param b
 	 * @return 2d Array
 	 */
-	public static int[][] normalize(int[] a, int[] b){
+	public static int[][] padZeros(int[] a, int[] b){
 		int[][] ordered = new int[2][];
 		int diff = 0;
 		
@@ -539,29 +575,32 @@ public class HPN {
 	}
 	
 	public static void checkNegative(HPN a) {
-		if(!a.negative && a.intPart < 0) {
-			a.negative = true; 
+		if(!a.isNegative && a.intPart < 0) {
+			a.isNegative = true; 
 			a.intPart *= -1; 
 		}
 	}
 	
 	public static HPN negate(HPN a) {
-		if(a.negative) {
-			a.negative = false;
+		if(a.isNegative) {
+			a.isNegative = false;
 		}
 		else {
-			a.negative = true;
+			a.isNegative = true;
 		}
 		
 		return a;
 	}
 	
+	
 	public static HPN copy(HPN a) {
 		HPN b = new HPN(a.intPart);
+		b.isExact = a.isExact;
 		b.fracPart = a.fracPart;
-		b.negative = a.negative;
+		b.isNegative = a.isNegative;
 		b.error = a.error;
-		b.errorIsTruncated = a.errorIsTruncated;
+		b.isTruncated = a.isTruncated;
+		updatePrecision(a);
 		return b; 
 	}
 	
@@ -574,7 +613,7 @@ public class HPN {
 	public static int[] add(int[] a, int[] b) {
 
 		//processing
-		int addition[][] = normalize(a, b);
+		int addition[][] = padZeros(a, b);
 		int[] sum = new int[addition[0].length];
 		//initialize array for fracPart 
 		
@@ -608,7 +647,79 @@ public class HPN {
 		return sum;
 	}
 	
-	public static void resetNumCalculations() {
-		numCalculations = 0;
+	public static void updatePrecision(HPN a) {
+		a.precision = a.fracPart.length; 
+	}
+	
+	public static int calculatePrecision(HPN a, HPN b) {
+		int aP = a.precision;
+		int bP = b.precision;
+		
+		if(a.isExact || b.isExact) {
+			//max
+			if(aP > bP) {
+				return aP;
+			}else {
+				return bP;
+			}
+		}else {
+			//min
+			if(aP > bP) {
+				return bP;
+			}else {
+				return aP;
+			}
+		}
+	}
+	
+	public static void applyPrecision(HPN a) {
+		if(a.precision < a.fracPart.length) {
+			int[] replacement = new int[a.precision];
+			
+			for(int i = 0; i < replacement.length; i++) {
+				replacement[i] = a.fracPart[i]; 
+			}
+			
+			a.fracPart = replacement; 
+			a.isTruncated = true;
+		}
+	}
+	
+	//Getters & Setters
+	public int getInt() {
+		return this.intPart;
+	}
+	public void setInt(int intPart) {
+		this.intPart = intPart;
+	}
+	public int[] getFracPart() {
+		return this.fracPart;
+	}
+	public void setFracPart(int[] fracPart) {
+		this.fracPart = fracPart;
+	}
+	public int getError() {
+		return this.error;
+	}
+	public void setError(int error) {
+		this.error = error;
+	}
+	public boolean getIsNegative() {
+		return this.isNegative;
+	}
+	public void setIsNegative(boolean isNegative) {
+		this.isNegative = isNegative;
+	}
+	public boolean getErrorIsTruncated() {
+		return this.isTruncated;
+	}
+	public void setErrorIsTruncated(boolean isTruncated) {
+		this.isTruncated = isTruncated;
+	}
+	public errorData getErrorData() {
+		return this.errorData;
+	}
+	public void setErrorData(errorData errorData) {
+		this.errorData = errorData;
 	}
 }
--- src/HPN_Test.java
+++ src/HPN_Test.java
@@ -5,18 +5,15 @@ public class HPN_Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		HPN x = new HPN("-1.998", 1);
-		HPN y = new HPN("-0.07", 1);
+		HPN x = new HPN(".123", true);
+		HPN y = new HPN(".1234", false);
 		
 		
-		HPN result = HPN.divide(x,2);
-		HPN.resetNumCalculations();
+		HPN result = HPN.multiply(x,0);
 		
 		System.out.println("---------------");
 		//System.out.println(error);
 		System.out.println(Objects.toString(result));
-		
 
 	}
-
 }
